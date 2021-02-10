91c74d2d67a7ecfe80bcfe23b84aef6299cad6fa
Alex Sumenko <alexsumenko@Alexs-MacBook-Pro.local>
2021-02-10 15:02:54 +0200

    add redirect to homepage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+
 import Dashboard from './ui/containers/dashboard/dashboard';
 
 import './App.scss';
@@ -6,7 +8,10 @@ import './App.scss';
 const App = () => {
     return (
         <div className='App'>
-            <Dashboard />
+            <Switch>
+                <Route exact path='/' component={Dashboard} />
+                <Redirect to='/' />
+            </Switch>
         </div>
     );
 };
